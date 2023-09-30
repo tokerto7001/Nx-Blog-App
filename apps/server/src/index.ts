4a5bb939b89eaee958e9b2af0daf846119ba0b17
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2023-09-30 11:41:02 +0300

    build: create a server and its dependencies

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('server');
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { determineEvPath, TNodeEnv } from './utilities/determineEnvPath';
+
+dotenv.config({ path: determineEvPath(process.env.NODE_ENV as TNodeEnv) });
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('ok');
+});
+
+app.listen(() => {
+    console.log(`Server is awake on port ${process.env.PORT}`);
+});
