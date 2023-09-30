2c578b97fb48ff5983fcc42e7653c2835ea1b2b4
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2023-09-30 13:54:28 +0300

    build: add jest and supertest for testing

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,12 +3,19 @@ import dotenv from 'dotenv';
 import { determineEvPath, TNodeEnv } from './utilities/determineEnvPath';
 
 dotenv.config({ path: determineEvPath(process.env.NODE_ENV as TNodeEnv) });
-const app = express();
+export const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.status(200).send('ok');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (req: Request, res: Response) => {
+    if (!req.body.test) {
+        res.status(400).send();
+    } else {
+        res.status(200).send('ok');
+    }
 });
 
-app.listen(() => {
+export const httpServer = app.listen(process.env.PORT, () => {
     console.log(`Server is awake on port ${process.env.PORT}`);
 });
