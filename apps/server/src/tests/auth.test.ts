d4f91e9fc924a26f682420cb9d143eee014b7f6a
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2023-11-10 13:17:17 +0300

    test: add verify user test

--- apps/server/src/tests/auth.test.ts
+++ apps/server/src/tests/auth.test.ts
@@ -2,6 +2,10 @@ import { client, db } from '../clients/postgres';
 import { Users } from '../db/schema';
 import { httpServer } from '../index';
 import request from 'supertest';
+import { eq } from 'drizzle-orm';
+import { generateEncodedString } from '../utilities/generateEncodedString';
+
+const { JWT_AUTH_SECRET_KEY, JWT_AUTH_EXPIRES_IN } = process.env;
 
 const server = request(httpServer);
 
@@ -20,21 +24,47 @@ const userRegisterObject: IUserRegisterObject = {
 };
 
 describe('Authentication process test', () => {
-    it('should returns 400 if required properties are not provided', async () => {
-        await server
-            .post('/api/users/register')
-            .send({
-                fullName: userRegisterObject.fullName,
-                email: userRegisterObject.email,
-            })
-            .expect(400);
+    describe('Register', () => {
+        it('should returns 400 if required properties are not provided', async () => {
+            await server
+                .post('/api/users/register')
+                .send({
+                    fullName: userRegisterObject.fullName,
+                    email: userRegisterObject.email,
+                })
+                .expect(400);
+        });
+
+        it('should successfully register the user', async () => {
+            await server
+                .post('/api/users/register')
+                .send(userRegisterObject)
+                .expect(201);
+        });
     });
 
-    it('should successfully register the user', async () => {
-        await server
-            .post('/api/users/register')
-            .send(userRegisterObject)
-            .expect(201);
+    describe('Verify User', () => {
+        it('should return 400 if verification code is not provided properly', async () => {
+            await server.get('/api/users/verify/test').send().expect(400);
+        });
+
+        it('should verify the user', async () => {
+            const registeredUser = await db
+                .select()
+                .from(Users)
+                .where(eq(Users.email, userRegisterObject.email));
+
+            const verificationCode = await generateEncodedString(
+                { userId: registeredUser[0].id },
+                JWT_AUTH_SECRET_KEY,
+                JWT_AUTH_EXPIRES_IN,
+            );
+
+            await server
+                .get(`/api/users/verify/${verificationCode}`)
+                .send()
+                .expect(200);
+        });
     });
 });
 
